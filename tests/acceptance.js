e5663c694010f9af804d892f62ceba21e7f7ac48
Felipe Knorr Kuhn <git@knorrium.info>
2012-09-09 19:27:22 -0300

    Adds a test to the getElementText method

--- tests/acceptance.js
+++ tests/acceptance.js
@@ -1,28 +1,37 @@
+/*globals console */
 var webdriver = require('wd'),
-assert = require('assert');
+    assert = require('assert');
 
 var browser = webdriver.remote("localhost", 8080);
 
-browser.on('status', function(info){
-  console.log('\x1b[36m%s\x1b[0m', info);
+browser.on('status', function (info) {
+    console.log('\x1b[36m%s\x1b[0m', info);
 });
 
-browser.on('command', function(meth, path){
-  console.log(' > \x1b[33m%s\x1b[0m: %s', meth, path);
+browser.on('command', function (meth, path) {
+    console.log(' > \x1b[33m%s\x1b[0m: %s', meth, path);
 });
 
 browser.init({
-    browserName:'firefox',
+    browserName: 'firefox',
     tags : ["examples"],
     name: "This is an example test"
-}, function() {
-    browser.elementByCssSelector('.win-item', function(err, el) {
-        if(err) {
+}, function () {
+    browser.elementByCssSelector('.win-item', function (err, el) {
+        if (err) {
             console.log(err);
             return;
         }
-        el.click(function() {
-
+        el.click(function () {
+            browser.elementByCssSelector(".pagetitle", function (err, e2) {
+                if (err) {
+                    console.log(err);
+                    return;
+                }
+                e2.textPresent("Group Title: 1", function (err, present) {
+                    assert.ok(present, "Not on the right page");
+                });
+            });
         });
     });
 });
